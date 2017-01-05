4d510eadde3e8d1e683e4f68e8a15bed6ea96320
Tao Ren <taoren.physics@icloud.com>
2017-01-05 15:25:00 -0800

    Linking the form to the backend v2

--- dbhelper.py
+++ dbhelper.py
@@ -50,7 +50,7 @@ class DBHelper:
 			with connection.cursor() as cursor:
 				cursor.execute(query, (category, date, latitude, longitude, description))
 				connection.commit()
-			except Exception as e:
-				print(e)
-			finally:
-				connection.close()
+		except Exception as e:
+			print(e)
+		finally:
+			connection.close()
